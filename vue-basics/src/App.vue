08d12c10c7507cb64c5393ef8f79d3dcc22668ad
snacccc <80086013+snacccc@users.noreply.github.com>
2022-10-31 13:14:27 -0400

    updated nav bar

--- vue-basics/src/App.vue
+++ vue-basics/src/App.vue
@@ -1,15 +1,13 @@
-<script setup>
-
-</script>
-
 <template>
-  <header>
+  <header class="header">
  
     <div class="app">
       <div class="nav">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/yes">...</RouterLink>
+        <RouterLink class="router home" to="/">Home</RouterLink>
+        <header>|</header>
+        <RouterLink class="router about" to="/about">About</RouterLink>
+        <header>|</header>
+        <RouterLink class="router yes" to="/yes">...</RouterLink>
       </div>
     </div>
   </header>
@@ -19,18 +17,42 @@
 
 <style scoped>
 
+  .header {
+    background-color: #3b2e4a;
+  }
   .app {
     margin-bottom: 1rem;
     padding-left: 1rem;
     padding-right: 1rem;
+  }  
+  
+  a {
+    text-decoration: none;
+    color: inherit;
   }
   .nav {
     display: flex;
     align-content: center;
-    justify-content: space-between;
-    padding: 1rem;
-    width: 30rem;
+    padding: 2rem;
+    margin: 0rem;
     font-size: 2rem;
+    color:#c9c9c9
+  }
+
+  .router + header {
+    margin-left: 5rem;
+  }
+
+  header + .router {
+    margin-left: 5rem;
+  }
+
+  .router:hover {
+    text-decoration: underline;
+  }
+
+  .router:active {
+    color: #407de4;
   }
 
 </style>
